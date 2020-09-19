a7686e101ac4395fca6118f3f02430b08364815d
roman <romka69tver@gmail.com>
2020-09-20 01:20:26 +0300

    add relation user-courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+user = User.create!(email: "a@a.a", password: "123456", password_confirmation: "123456")
+
 30.times do
   Course.create!([{
                       title: Faker::Educator.course_name,
-                      description: Faker::TvShows::GameOfThrones.quote
+                      description: Faker::TvShows::GameOfThrones.quote,
+                      user_id: user.id
                   }])
 end
