60b0ce01c4552399748105a738dfe02c7a8e9e2a
roman <romka69tver@gmail.com>
2020-09-22 22:16:34 +0300

    fix mailer

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
+    :domain => 'udem-rails.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
