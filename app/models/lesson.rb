3bb82260b3af4adc1642418ddb2f5e394d909911
roman <romka69tver@gmail.com>
2020-09-29 20:05:26 +0300

    add activity with styles for lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,7 @@ class Lesson < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 end
