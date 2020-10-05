22161ddaf0e91e70ef32f7183355f51026bc5a4b
roman <romka69tver@gmail.com>
2020-10-05 15:42:24 +0300

    add friendly id for enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -57,7 +57,7 @@ class EnrollmentsController < ApplicationController
 
   private
     def set_enrollment
-      @enrollment = Enrollment.find(params[:id])
+      @enrollment = Enrollment.friendly.find(params[:id])
     end
 
     def set_course
