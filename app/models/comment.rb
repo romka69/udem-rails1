4cd28e39be637e3a8f94e90f6603a3d3201b0981
roman <romka69tver@gmail.com>
2020-11-05 23:36:05 +0300

    add comment to activity track

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :lesson, counter_cache: true
 
   validates :content, presence: true
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 end
