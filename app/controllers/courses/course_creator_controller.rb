3adb6e1db195b601fa88c921d21a8f0ebf4e7458
roman <romka69tver@gmail.com>
2020-11-14 04:13:55 +0300

    add styling for form

--- app/controllers/courses/course_creator_controller.rb
+++ app/controllers/courses/course_creator_controller.rb
@@ -1,18 +1,40 @@
 class Courses::CourseCreatorController < ApplicationController
   include Wicked::Wizard
 
-  before_action :set_progress, only: :show
+  before_action :set_progress, only: %i[show update]
+  before_action :set_course, only: %i[show update finish_wizard_path]
 
   steps :basic_info, :details
 
   def show
-    @course = Course.friendly.find(params[:course_id])
+    authorize @course, :edit?
+
+    case step
+    when :basic_info
+    when :details
+      @tags = Tag.all
+    end
 
     render_wizard
   end
 
+  def update
+    authorize @course, :edit?
+
+    case step
+    when :basic_info
+      @course.update_attributes(course_params)
+    when :details
+      @tags = Tag.all
+      @course.update_attributes(course_params)
+    end
+
+    render_wizard @course
+  end
+
   def finish_wizard_path
-    @course = Course.friendly.find(params[:course_id])
+    authorize @course, :edit?
+
     course_path(@course)
   end
 
@@ -24,5 +46,14 @@ class Courses::CourseCreatorController < ApplicationController
     else
       @progress = 0
     end
+  end
+
+  def set_course
+    @course = Course.friendly.find(params[:course_id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :short_description,
+                                   :price, :level, :language, :published, :logo, tag_ids: [])
   end
 end
